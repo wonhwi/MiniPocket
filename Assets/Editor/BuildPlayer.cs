e80a303e27e58c1ba48c9b76e86ebea59e1b3dff
Hwi <wjddnjsgnl159@gmail.com>
2024-04-26 06:01:22 +0900

    Add Jenkins Script
    Add Jenkins Script

---
+++ Assets/Editor/BuildPlayer.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using System.Linq;
+using UnityEditor.Build.Reporting;
+
+public class BuildPlayer : MonoBehaviour
+{
+  [MenuItem("Build/Standalone Windows")]
+  public static void Build_AOS_Debug()
+  {
+    BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions
+    {
+      scenes = GetScenes(),
+      locationPathName = $"Builds/MiniPocket_{PlayerSettings.bundleVersion}.exe",
+      target = BuildTarget.StandaloneWindows
+    };
+
+    BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+
+    Debug.Log(GetResult(report.summary));
+
+  }
+
+
+
+  static string[] GetScenes()
+    => EditorBuildSettings.scenes.Where(n => n.enabled).Select(m => m.path).ToArray();
+
+  static string GetResult(BuildSummary buildSummary) => buildSummary.result switch
+  {
+    BuildResult.Succeeded
+      => $"Build succeeded: {buildSummary.totalSize} bytes in {buildSummary.totalTime}",
+
+    BuildResult.Unknown
+    or BuildResult.Failed
+    or BuildResult.Cancelled
+    or _
+      => $"Build {buildSummary.result} totalErrors = {buildSummary.totalErrors}",
+  };
+
+}
